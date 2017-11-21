90f981d101f86bcf70e011e725ff4cfe9b6f7145
Nathael Arki <nathael.arki@supinternet.fr>
2017-11-21 12:15:42 +0100

    remove old models comment some code

--- db.php
+++ db.php
@@ -12,8 +12,8 @@ $conn = [
     "password" => "algo"
 ];
 
-Film::has(['Seance' => 'film_id']);
-Cinema::has(['Seance' => 'cinema_id']);
+// Film::has(['Seance' => 'film_id']);
+// Cinema::has(['Seance' => 'cinema_id']);
 
 $manager = DB::getInstance();
 $manager->addConnection($conn);
